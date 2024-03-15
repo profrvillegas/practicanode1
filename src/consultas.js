a04317f83ae6842dbea45e9107102177d879cef4
Rafael Villegas <123702862+profrvillegas@users.noreply.github.com>
2024-03-15 18:33:04 -0500

    lista de clientes y busqueda por id

--- src/consultas.js
+++ src/consultas.js
@@ -1,9 +1,22 @@
-const conexion=require('./conexion');
+const conexion = require('./conexion');
 
-const getClientes=async ()=>{
-    const [consulta]=await conexion.
-    execute('select * from clientes');
+const getClientes = async () => {
+    const [consulta] = await conexion.
+        execute('select * from clientes');
     return consulta;
 };
 
-module.exports={ getClientes}
+// metodo para buscar un cliente por id
+const getClienteById = async (id) => {
+    const [consulta] = await conexion.execute('SELECT * FROM clientes WHERE id_cliente=?', [id]);
+    return consulta;
+}
+
+// metodo para insertar nuevos clientes
+const insertCliente = async (nombre_cliente, correo_electronico, contrasenia) => {
+    const [consulta] = await conexion.execute('INSERT INTO clientes(nombre_cliente,correo_electronico,contrasenia) VALUES(?,?,?)',
+        [nombre_cliente, correo_electronico, contrasenia]);  
+    return consulta;
+}
+
+module.exports = { getClientes, getClienteById, insertCliente }
--- src/rutas/clientes.js
+++ src/rutas/clientes.js
@@ -1,9 +1,28 @@
-const {Router}=require('express');
-const consultas=require('../consultas');
-const router=Router();
-router.get('/',async(req, res)=>{
-    const consulta=await consultas.getClientes();
+const { Router, json } = require('express');
+const consultas = require('../consultas');
+const router = Router();
+// ruta para acceder a todos los clientes
+router.get('/', async (req, res) => {
+    const consulta = await consultas.getClientes();
     return res.status(200).json(consulta);
 })
 
-module.exports=router;
+// ruta para acceder al detalle de un cliente
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const consulta = await consultas.getClienteById(id);
+    if (consulta.length === 0) {
+        return res.status(400).json({ mensaje: "cliente no encontrado" });
+    }
+    return res.status(200).json(consulta);
+});
+
+// ruta para agregar un nuevo cliente
+router.post('/', async (req, res) => {
+    const { nombre_cliente, correo_electronico, contrasenia } = req.body;
+    const consulta = await consultas.insertCliente(nombre_cliente,
+        correo_electronico, contrasenia);
+    return res.status(200).json(consulta);
+});
+
+module.exports = router;
